b53b502e913d079df092deb1bc32211b184b4511
Sean Jacobson <sean.jacobson86@gmail.com>
2021-12-16 17:19:31 -0500

    Update navbar.js

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -22,16 +22,16 @@ export default function Navigation() {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className='mr-auto'>
-                            <Nav.Link className='nav-home'>Home</Nav.Link>
-                            <NavDropdown title='About Us' className='nav-about'>
+                            <Nav.Link className='nav-home link'>Home</Nav.Link>
+                            <NavDropdown title='About Us' className='nav-about link'>
                                 <NavDropdown.Item>Who We Are</NavDropdown.Item>
                                 <NavDropdown.Item>Our Mission</NavDropdown.Item>
                                 <NavDropdown.Item>Constitution</NavDropdown.Item>
                             </NavDropdown>
-                            <Nav.Link className='nav-get'>Get Involved</Nav.Link>
-                            <Nav.Link className='nav-resources'>Resources</Nav.Link>
-                            <Nav.Link className='nav-donate'>Donate</Nav.Link>
-                            <Nav.Link className='nav-contact'>Contact</Nav.Link>
+                            <Nav.Link className='nav-get link'>Get Involved</Nav.Link>
+                            <Nav.Link className='nav-resources link'>Resources</Nav.Link>
+                            <Nav.Link className='nav-donate link'>Donate</Nav.Link>
+                            <Nav.Link className='nav-contact link'>Contact</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>

1658ab96cc63124f25ba5c2f8dba90736596f1a0
Sean Jacobson <sean.jacobson86@gmail.com>
2021-12-16 19:40:00 -0500

    Update teamCards.js

--- client/src/components/teamCards/teamCards.js
+++ client/src/components/teamCards/teamCards.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Container } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 
 //Styling
 import "./teamCards.css";
@@ -9,23 +9,46 @@ export default function TeamCards() {
         <div>
             <h1 className="centerText">Meet Our Team</h1>
             <Container>
-                <Card className='teamCards'>
-                    <Card.Img variant="top" src="./Images/charley.jpeg" />
-                    <Card.Body>
-                        <Card.Title className="centerText cardTitle">
-                            Charley
-                        </Card.Title>
-                        <Card.Subtitle className="centerText cardMargins cardSubtitle">
-                            Title:
-                        </Card.Subtitle>
-                        <Card.Subtitle className="centerText cardSubtitle">
-                            Pronouns:
-                        </Card.Subtitle>
-                        <Card.Text className='cardText'>
-                            This is a test
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+                <Row>
+                    <Col xs={12} md={4} className='cardColumns'>
+                        <Card className='teamCards'>
+                            <Card.Img variant="top" src="./Images/charley.jpeg" />
+                            <Card.Body>
+                                <Card.Title className="centerText cardTitle">
+                                    Charley
+                                </Card.Title>
+                                <Card.Subtitle className="centerText cardMargins cardSubtitle">
+                                    Title:
+                                </Card.Subtitle>
+                                <Card.Subtitle className="centerText cardSubtitle">
+                                    Pronouns:
+                                </Card.Subtitle>
+                                <Card.Text className='cardText'>
+                                    This is a test
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col xs={12} md={4}>
+                        <Card className='teamCards'>
+                            <Card.Img variant="top" src="./Images/charley.jpeg" />
+                            <Card.Body>
+                                <Card.Title className="centerText cardTitle">
+                                    Charley
+                                </Card.Title>
+                                <Card.Subtitle className="centerText cardMargins cardSubtitle">
+                                    Title:
+                                </Card.Subtitle>
+                                <Card.Subtitle className="centerText cardSubtitle">
+                                    Pronouns:
+                                </Card.Subtitle>
+                                <Card.Text className='cardText'>
+                                    This is a test
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
             </Container>
         </div>
     );

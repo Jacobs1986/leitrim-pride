6e48ad6dbeeeb4e29ce3bb72b7baccff5eee1ac8
Sean Jacobson <sean.jacobson86@gmail.com>
2021-12-17 10:39:56 -0500

    Update contact.js

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 export default function Contact() {
     return (
-        <div>This page is ready!</div>
+        <div>
+            <Container>
+                <Row>
+                    <Col lg={12}>
+                        We love hearing from you. Please send us a message!
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 };

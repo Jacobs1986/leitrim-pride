5bec7121a6f30fc5721100dc572e7b5060f64d8a
Sean Jacobson <sean.jacobson86@gmail.com>
2021-12-17 11:02:54 -0500

    Setting the styles

--- client/src/components/contactForm/contactForm.js
+++ client/src/components/contactForm/contactForm.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
 
 //Styling
-// import "./contactForm.css";
+import "./contactForm.css";
 
 export default function ContactForm() {
     return (

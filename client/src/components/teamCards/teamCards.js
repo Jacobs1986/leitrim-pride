b563a7986f1093b845cc48e3e0b2a182b72d4a97
Sean Jacobson <sean.jacobson86@gmail.com>
2021-12-16 19:23:00 -0500

    Update teamCards.js

--- client/src/components/teamCards/teamCards.js
+++ client/src/components/teamCards/teamCards.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Card, Container } from "react-bootstrap";
 
 //Styling
 import "./teamCards.css";
@@ -7,6 +8,16 @@ export default function TeamCards() {
     return (
         <div>
             <h1 className="centerText">Meet Our Team</h1>
+            <Container>
+                <Card className='teamCards'>
+                    <Card.Img variant="top" src="./Images/charley.jpeg" />
+                    <Card.Body>
+                        <Card.Title className="centerText">
+                            Charley
+                        </Card.Title>
+                    </Card.Body>
+                </Card>
+            </Container>
         </div>
     );
 };

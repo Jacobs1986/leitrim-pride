8c57fc8808c2d6dcb9a7de0db2fd5f80dd0bf939
Sean Jacobson <sean.jacobson86@gmail.com>
2021-12-27 12:52:56 -0500

    Update comingSoonBanner.js

--- client/src/components/comingSoonBanner/comingSoonBanner.js
+++ client/src/components/comingSoonBanner/comingSoonBanner.js
@@ -3,8 +3,19 @@ import React from "react";
 //Styling
 import "./comingSoonBanner.css";
 
+// Bootstrap
+import { Col, Container, Row } from "react-bootstrap";
+
 export default function ComingSoonBanner() {
     return (
-        <div>This component is ready!</div>
+        <div>
+            <Container>
+                <Row>
+                    <Col xs={12} className="centerText">
+                        COMING SOON!
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 };

b85dd52677815d41bcaf05f72e7f82c151df5fbb
Sean Jacobson <sean.jacobson86@gmail.com>
2021-12-27 12:36:08 -0500

    Update coming-soon-page.js

--- client/src/pages/coming-soon-page.js
+++ client/src/pages/coming-soon-page.js
@@ -2,11 +2,13 @@ import React from "react";
 
 // Components
 import ComingSoonImage from "../components/comingSoonImage/comingSoonImage";
+import ComingSoonBanner from "../components/comingSoonBanner/comingSoonBanner";
 
 
 export default function ComingSoon() {
     return (
         <div>
+            <ComingSoonBanner />
             <ComingSoonImage />
         </div>
     );

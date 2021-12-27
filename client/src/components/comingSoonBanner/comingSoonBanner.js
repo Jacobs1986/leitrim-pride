a01a3db04a82c16a092896a61ced9c082fc9cec1
Sean Jacobson <sean.jacobson86@gmail.com>
2021-12-27 12:34:52 -0500

    new component

---
+++ client/src/components/comingSoonBanner/comingSoonBanner.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//Styling
+import "./comingSoonBanner.css";
+
+export default function ComingSoonBanner() {
+    return (
+        <div>This component is ready!</div>
+    );
+};

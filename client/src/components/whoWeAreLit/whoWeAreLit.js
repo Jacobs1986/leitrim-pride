165b685ce975c067ca47b25cdfc4912b9c0bc0fc
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-13 11:54:00 -0400

    Made WhoWeAreLit component, both js and css files

---
+++ client/src/components/whoWeAreLit/whoWeAreLit.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// CSS File
+import "./whoWeAreLit.css";
+
+export default function WhoWeAreLit() {
+    return (
+        <div>About literature will go here!</div>
+    );
+};

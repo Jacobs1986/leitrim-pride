5b1960d8d5fd88c1da13d448b931334a92959a89
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-13 10:25:57 -0400

    I'm going to be adding a new banner using the pictures from the google drive folder.

--- client/src/components/banner/banner.js
+++ client/src/components/banner/banner.js
@@ -9,14 +9,17 @@ import { Container, Image } from 'react-bootstrap';
 export default function Banner() {
     return (
         <div>
-            <Container className='bannerContainer' fluid>
-                <Image
-                    src='./Images/banner.jpeg'
-                    alt='banner'
-                    width='100%'
-                />
-                <div className='overlay'>A FIRST TIME FOR EVERYTHING</div>
-            </Container>
+            New banner goes here
         </div>
+        // <div>
+        //     <Container className='bannerContainer' fluid>
+        //         <Image
+        //             src='./Images/banner.jpeg'
+        //             alt='banner'
+        //             width='100%'
+        //         />
+        //         <div className='overlay'>A FIRST TIME FOR EVERYTHING</div>
+        //     </Container>
+        // </div>
     );
 };

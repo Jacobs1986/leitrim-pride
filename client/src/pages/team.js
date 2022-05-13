698a37390085806701f737f109b49ef0a52a6ea4
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-13 11:25:48 -0400

    Making a new page called team

---
+++ client/src/pages/team.js
@@ -0,0 +1,10 @@
+import React from "react";
+import TeamCards from "../components/teamCards/teamCards";
+
+export default function Team() {
+    return (
+        <div>
+            <TeamCards />
+        </div>
+    );
+};

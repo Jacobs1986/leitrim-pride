792718fb0838d60380849105a7110ad131deb5d4
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-13 11:05:46 -0400

    Copied and pasted literature from the document into two paragraph tag

--- client/src/components/homePageLit/homePageLit.js
+++ client/src/components/homePageLit/homePageLit.js
@@ -3,8 +3,25 @@ import React from "react";
 // CSS File
 import "./homePageLit.css";
 
+// Bootstrap
+import {
+    Container,
+    Row,
+    Col
+} from 'react-bootstrap';
+
 export default function HomePageLit() {
     return (
-        <div>This is the homepage literature</div>
+        <div>
+            <Container>
+                <Row>
+                    <Col xs={12}>
+                        <p>Leitrim Pride seeks to bring together individuals and organisations to empower, educate and support the Leitrim LGBTQI+ community through the production of an annual community LGBTQI+ Pride Festival and events throughout the year.</p>
+
+                        <p>Leitrim Pride envisions a unified community where diverse individuals are celebrated and are able to thrive as their authentic selves.</p>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 };

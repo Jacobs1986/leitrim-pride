7e22aa6b9668d7f7cf8f33c5eb2de78ff32eef99
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-13 11:12:04 -0400

    Created a className for the p tags so that the styling will not affect any of the other p tags on the site

--- client/src/components/homePageLit/homePageLit.js
+++ client/src/components/homePageLit/homePageLit.js
@@ -16,9 +16,8 @@ export default function HomePageLit() {
             <Container>
                 <Row>
                     <Col xs={12}>
-                        <p>Leitrim Pride seeks to bring together individuals and organisations to empower, educate and support the Leitrim LGBTQI+ community through the production of an annual community LGBTQI+ Pride Festival and events throughout the year.</p>
-
-                        <p>Leitrim Pride envisions a unified community where diverse individuals are celebrated and are able to thrive as their authentic selves.</p>
+                        <p className="homePage-p">Leitrim Pride seeks to bring together individuals and organisations to empower, educate and support the Leitrim LGBTQI+ community through the production of an annual community LGBTQI+ Pride Festival and events throughout the year.</p>
+                        <p className="homePage-p">Leitrim Pride envisions a unified community where diverse individuals are celebrated and are able to thrive as their authentic selves.</p>
                     </Col>
                 </Row>
             </Container>

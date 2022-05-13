805ea34ba245705253302b6d7fafa0cab65df6ec
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-13 10:59:30 -0400

    Created components for the homepage literature

---
+++ client/src/components/homePageLit/homePageLit.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// CSS File
+import "./homePageLit.css";
+
+export default function HomePageLit() {
+    return (
+        <div>This is the homepage literature</div>
+    );
+};

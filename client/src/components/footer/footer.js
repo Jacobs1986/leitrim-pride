5e05748d4f7174e52c7126c99fb5937b1fd07a85
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-16 10:45:42 -0400

    Imported Bootstrap; Created some columns for the content

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -3,10 +3,29 @@ import React from "react";
 // CSS File
 import "./footer.css";
 
+// Bootstrap
+import {
+    Container,
+    Row,
+    Col
+} from 'react-bootstrap'
+
 export default function Footer() {
     return (
-        <footer>
-            This is the footer
+        <footer className="centerText">
+            <Container>
+                <Row>
+                    <Col lg={4}>
+                        Column 1
+                    </Col>
+                    <Col lg={4}>
+                        Column 2
+                    </Col>
+                    <Col lg={4}>
+                        Column 3
+                    </Col>
+                </Row>
+            </Container>
         </footer>
     );
 };

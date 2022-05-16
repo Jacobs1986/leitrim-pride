5f1a6d086093c7cbac09a85cf5e01fe59cd77d77
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-16 14:27:36 -0400

    Removed the bootstrap Col and Row

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // Styling
-import { Col, Container, Row } from "react-bootstrap";
+import { Container  } from "react-bootstrap";
 
 // Components
 import ContactForm from "../components/contactForm/contactForm";

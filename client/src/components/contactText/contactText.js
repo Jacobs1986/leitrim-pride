94d02de327d8def9cae8b048f89edf7dfe1bb1f2
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-16 14:30:33 -0400

    Added some text for the contact page

--- client/src/components/contactText/contactText.js
+++ client/src/components/contactText/contactText.js
@@ -3,10 +3,29 @@ import React from "react";
 // CSS File
 import "./contactText.css";
 
+// Bootstrap
+import {
+    Row,
+    Col
+} from 'react-bootstrap'
+
 export default function ContactText() {
     return (
         <div>
             <h1 className="centerText">Contact Us</h1>
+            <Row>
+                <Col>
+                    <p>
+                        We love hearing from people in our community. Our goal is to provide resources and support that allows you to celebrate the wonderful uniqueness that is you. Please take a moment to send us a message letting us know how we are doing.
+                    </p>
+                    <p>
+                        As a nonprofit we rely on community members to help us put on events. We’re always looking for new faces to help out. Send us a message to let us know how you can get involved!
+                    </p>
+                    <p>
+                        And as always don’t forget to follow us on our social media:
+                    </p>
+                </Col>
+            </Row>
         </div>
     );
 };

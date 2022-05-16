41bbd54a3485cb658be1204417f520648883ca5a
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-16 12:07:39 -0400

    The banner I had made I didn't like the cropping so I just put in the original banner and I think it looks better

--- client/src/components/banner/banner.js
+++ client/src/components/banner/banner.js
@@ -11,7 +11,7 @@ export default function Banner() {
         <div>
             <Container className='bannerContainer' fluid>
                 <Image
-                    src={require('./homePageBanner.jpg')}
+                    src={require('./homePageBannerOrg.jpg')}
                     alt='banner'
                     width='100%'
                 />

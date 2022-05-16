639d93b1922e74dfeb6408067e659c2662be70c7
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-16 16:47:18 -0400

    Finished putting the line breaks into the bios

--- client/src/components/teamCards/teamCards.js
+++ client/src/components/teamCards/teamCards.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Col, Container, Image, Row } from "react-bootstrap";
+import { Col, Container, Image, Row } from "react-bootstrap";
 
 //Styling
 import "./teamCards.css";

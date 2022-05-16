4ba15b3cf88797d9b009124756f4bc873126e60a
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-16 10:38:35 -0400

    Changed the div tage to a footer one

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -5,6 +5,8 @@ import "./footer.css";
 
 export default function Footer() {
     return (
-        <div>This is the footer</div>
+        <footer>
+            This is the footer
+        </footer>
     );
 };

c69977060f75cbdae5504ed7479b7e6121f207c9
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-17 09:29:04 -0400

    Imported EventsPage component

--- client/src/pages/events.js
+++ client/src/pages/events.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+// Components
+import EventsPage from "../components/eventsPage/eventsPage";
+
 export default function Events() {
     return (
         <div>
             <h1 className="centerText">Upcoming Events</h1>
+            <EventsPage />
         </div>
     );
 };

fc23597543b073739d12190cf4f6984cc98bee6e
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-17 15:12:28 -0400

    Added some code for the construction page

---
+++ client/src/components/constructionPage/constructionPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// CSS File
+import "./constructionPage.css";
+
+export default function ConstructionPage() {
+    return (
+        <div>This is the construction component</div>
+    );
+};

b2746883674476a05622f4fc6094a1c3baea079b
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-17 09:57:34 -0400

    Added in some more breaks for columns

--- client/src/components/eventsPage/eventsPage.js
+++ client/src/components/eventsPage/eventsPage.js
@@ -16,14 +16,14 @@ export default function EventsPage() {
         <div className='eventsDiv'>
             <Container>
                 <Row>
-                    <Col xs={12} sm={12} md={6}>
+                    <Col xs={12} sm={12} md={12} lg={6}>
                         <Image 
                             src='./Images/Coming-Soon-PNG-Image.png'
                             alt='Coming Soon sign'
                             className='comingSoonImage'
                         />
                     </Col>
-                    <Col xs={12} sm={12} md={6}>
+                    <Col xs={12} sm={12} md={12} lg={6}>
                         <p className='eventsComingSoonText'>
                             We have some exciting events coming soon. Check back later to find out what they are!
                         </p>

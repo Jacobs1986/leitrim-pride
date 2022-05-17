886ea2b7fed78a2f69913eb00c5047163393723d
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-17 09:41:03 -0400

    Finished putting in code for EventsPage

--- client/src/components/eventsPage/eventsPage.js
+++ client/src/components/eventsPage/eventsPage.js
@@ -3,8 +3,32 @@ import React from "react";
 // CSS File
 import "./eventsPage.css";
 
+// Bootstrap
+import {
+    Container,
+    Row,
+    Col,
+    Image
+} from 'react-bootstrap';
+
 export default function EventsPage() {
     return (
-        <div>This is the events component</div>
+        <div>
+            <Container>
+                <Row>
+                    <Col md={6}>
+                        <Image 
+                            src='./Images/Coming-Soon-PNG-Image.png'
+                            alt='Coming Soon sign'
+                        />
+                    </Col>
+                    <Col md={6}>
+                        <p className='eventsComingSoonText'>
+                            We have some exciting events coming soon. Check back later to find out what they are!
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 };

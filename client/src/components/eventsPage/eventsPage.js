1ae359726cc44abf60e58197f906d298777beaae
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-17 09:27:59 -0400

    Began putting the code into the component

---
+++ client/src/components/eventsPage/eventsPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// CSS File
+import "./eventsPage.css";
+
+export default function EventsPage() {
+    return (
+        <div>This is the events component</div>
+    );
+};

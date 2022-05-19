208f2672fefccc8602842e7b61e30fd43139217a
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-19 16:48:10 -0400

    Created some test code

---
+++ client/src/components/constitutionViewer/constitutionViewer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// CSS File
+import "./constitutionViewer.css";
+
+export default function ConstitutionViewer() {
+    return (
+        <div>Here you can view the constitution</div>
+    );
+};

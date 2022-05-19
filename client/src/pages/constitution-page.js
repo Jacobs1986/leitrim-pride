f047c690bc7d79c117508a0e20699242c3caaab4
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-19 16:43:14 -0400

    Put in some test code so that I can get a better understanding of the layout

--- client/src/pages/constitution-page.js
+++ client/src/pages/constitution-page.js
@@ -1,7 +1,28 @@
 import React from "react";
 
+// Bootstrap
+import {
+    Container,
+    Row,
+    Col
+} from 'react-bootstrap';
+
 export default function Constitution() {
     return (
-        <div>This is the constitution page!</div>
+        <div>
+            <Container>
+                <h3 className='centerText'>Our Constitution</h3>
+                <Row>
+                    <Col>
+                        The link to open a window to download the constitution.
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        The viewer for the PDF
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 };

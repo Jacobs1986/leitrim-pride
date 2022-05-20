7b4d5766cd7086a62309f6cb1874e728b736d9bc
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-20 09:49:30 -0400

    Creating the get invovled component

---
+++ client/src/components/getInvovled/getInvovled.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// CSS File
+import "./getInvolved.css";
+
+export default function GetInvovledText() {
+    return (
+        <div>This is the get involved component</div>
+    );
+};

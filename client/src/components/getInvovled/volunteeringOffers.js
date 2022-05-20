f6e5bb5d66c78dbb39d99257b707968b6b135dbc
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-20 10:21:47 -0400

    Added in offer list text and added some styling

--- client/src/components/getInvovled/volunteeringOffers.js
+++ client/src/components/getInvovled/volunteeringOffers.js
@@ -5,6 +5,19 @@ import "./getInvolved.css";
 
 export default function VolunteeringOffer() {
     return (
-        <div>This is the volunteering paragraph</div>
+        <div>
+            <h4 className='getInvolvedHeader'>Volunteering With Us Offers:</h4>
+            <ul>
+                <li className='offerList'>
+                    A Unique Experience – we deliver a county-wide Pride, which constantly works to be inclusive of rural LGBTI+ voices
+                </li>
+                <li className='offerList'>
+                    Rewarding – gain and develop valuable transferable skills and see your work help build the festival and the community;
+                </li>
+                <li className='offerList'>
+                    A Great Team – we work hard to make everyone feel part of Pride and we want to make sure that the Pride team fully reflects the LGBTI+ community in Leitrim; many of us are experienced Pride organisers.
+                </li>
+            </ul>
+        </div>
     );
 };

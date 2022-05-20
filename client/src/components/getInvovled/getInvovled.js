79580cb2b24f281279532623620a835d075f4809
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-20 09:56:02 -0400

    Imported Bootstrap styling

--- client/src/components/getInvovled/getInvovled.js
+++ client/src/components/getInvovled/getInvovled.js
@@ -3,10 +3,24 @@ import React from "react";
 // CSS File
 import "./getInvolved.css";
 
+// Bootstrap
+import {
+    Container,
+    Row,
+    Col
+} from 'react-bootstrap';
+
 export default function GetInvovledComp() {
     return (
         <div>
             <h3 className="centerText">Get Involved</h3>
+            <Container>
+                <Row>
+                    <Col>
+                        
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };

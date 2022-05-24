daf14a9564d5057dcb29b4bcd0fce0305f57b9fb
Sean Jacobson <sean.jacobson86@gmail.com>
2022-05-24 09:07:08 -0400

    Redid the code so that it will accept the ref of the form; put in a window reload after a message is successfully sent.

--- client/src/components/contactForm/contactForm.js
+++ client/src/components/contactForm/contactForm.js
@@ -1,8 +1,9 @@
-// import React, { useRef } from "react";
-import React, {
-    useReducer,
-    useState
-} from 'react';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+// import React, {
+//     useReducer,
+//     useState
+// } from 'react';
 
 // Bootstrap
 import {
@@ -11,7 +12,7 @@ import {
     Form,
     Row
 } from "react-bootstrap";
-// import emailjs from "@emailjs/browser";
+
 
 //Styling
 import "./contactForm.css";
@@ -20,56 +21,57 @@ import "./contactForm.css";
 // import { messages2 } from "../../utils/message-API";
 
 // Functions
-import { reducer } from './functions';
+// import { reducer } from './functions';
 
 export default function ContactForm() {
-    // const form = useRef()
-    const [messageInfo, setMessageInfo] = useReducer(reducer, {});
-    const [nameCheck, setNameCheck] = useState(false);
-    const [emailCheck, setEmailCheck] = useState(false);
-    const [subjectCheck, setSubjectCheck] = useState(false);
-    const [messageCheck, setMessageCheck] = useState(false);
+    const form = useRef()
+    // const [messageInfo, setMessageInfo] = useReducer(reducer, {});
+    // const [nameCheck, setNameCheck] = useState(false);
+    // const [emailCheck, setEmailCheck] = useState(false);
+    // const [subjectCheck, setSubjectCheck] = useState(false);
+    // const [messageCheck, setMessageCheck] = useState(false);
 
-    const handleMessageInput = event => {
-        setMessageInfo({
-            name: event.target.name,
-            value: event.target.value
-        })
-    }
+    // const handleMessageInput = event => {
+    //     setMessageInfo({
+    //         name: event.target.name,
+    //         value: event.target.value
+    //     })
+    // }
 
     const handleSendMessage = (event) => {
         event.preventDefault();
         // Check to see if the name field is blank
-        if (!messageInfo.from_name || messageInfo.from_name === "") {
-            setNameCheck(true);
-            return
-        }
-        if (!messageInfo.reply_to || messageInfo.reply_to === "") {
-            setEmailCheck(true);
-            return
-        }
-        if (!messageInfo.subject || messageInfo.subject === "---") {
-            setSubjectCheck(true);
-            return
-        }
-        if (!messageInfo.message || messageInfo.message === "") {
-            setMessageCheck(true);
-            return
-        }
+        // if (!messageInfo.from_name || messageInfo.from_name === "") {
+        //     setNameCheck(true);
+        //     return
+        // }
+        // if (!messageInfo.reply_to || messageInfo.reply_to === "") {
+        //     setEmailCheck(true);
+        //     return
+        // }
+        // if (!messageInfo.subject || messageInfo.subject === "---") {
+        //     setSubjectCheck(true);
+        //     return
+        // }
+        // if (!messageInfo.message || messageInfo.message === "") {
+        //     setMessageCheck(true);
+        //     return
+        // }
         // console.log(messageInfo);
         // Send the message
-        // emailjs.sendForm('service_rzc3jxe', 'template_32kveqk', form.current, 'hjTQGHelMqEMMP7I-')
-        //     .then((results) => {
-        //         console.log(results)
-        //         alert('Thank you for your message! We\'ll get back to you ASAP!')
-        //     }, (error) => {
-        //         console.log(error.text)
-        //     })
+        emailjs.sendForm('service_rzc3jxe', 'template_32kveqk', form.current, 'hjTQGHelMqEMMP7I-')
+            .then((results) => {
+                console.log(results)
+                alert('Thank you for your message! We\'ll get back to you ASAP!')
+                window.location.reload();
+            }, (error) => {
+                console.log(error.text)
+            })
     }
 
     return (
         <div id="contactDiv">
-            <Form onSubmit={handleSendMessage}>
+            {/* <Form onSubmit={handleSendMessage}>
                 <Row>
                     <Col xs={12} md={6} lg={6}>
                         <Form.Label>
@@ -142,14 +144,15 @@ export default function ContactForm() {
                         </Button>
                     </Col>
                 </Row>
-            </Form>
-            {/* <Form ref={form} onSubmit={handleSendMessage}>
+            </Form> */}
+            <Form ref={form} onSubmit={handleSendMessage}>
                 <Row>
                     <Col xs={12} md={6} lg={6}>
                         <Form.Label>
                             Your Name
                         </Form.Label>
                         <Form.Control
+                            required
                             name="from_name"
                         />
                     </Col>
@@ -158,6 +161,7 @@ export default function ContactForm() {
                             Your email
                         </Form.Label>
                         <Form.Control
+                            required
                             type="email"
                             name="reply_to"
                         />
@@ -169,14 +173,15 @@ export default function ContactForm() {
                             Subject
                         </Form.Label>
                         <Form.Control
+                            required
                             name="subject"
                             as='select'
-                            defaultValue="---"
+                            defaultValue="Volunteer Opportunities"
                         >
-                            <option>---</option>
                             <option>Volunteer Opportunities</option>
                             <option>Event Questions</option>
                             <option>Kudos</option>
+                            <option>Other</option>
                         </Form.Control>
                     </Col>
                 </Row>
@@ -186,6 +191,7 @@ export default function ContactForm() {
                             Write us a message!
                         </Form.Label>
                         <Form.Control
+                            required
                             as="textarea"
                             rows='6'
                             name="message"
@@ -202,7 +208,7 @@ export default function ContactForm() {
                         </Button>
                     </Col>
                 </Row>
-            </Form> */}
+            </Form>
         </div>
     );
 };

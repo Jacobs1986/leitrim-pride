4f22eefe1ffbe4daf6fedab8a765a47a0da113ad
Sean Jacobson <sean.jacobson86@gmail.com>
2023-09-20 12:13:29 -0600

    changed margins to padding in layout.css; added content to literature

--- src/components/homePage/literature.js
+++ src/components/homePage/literature.js
@@ -5,6 +5,17 @@ import "./homePage-styles.css";
 
 export default function HomePageLit() {
     return (
-        <div>This is the literature component.</div>
+        <div className="container">
+            <div className="row">
+                <div className="col-xl-12">
+                    <p>
+                        Leitrim Pride seeks to bring together individuals and organisations to empower, educate and support the Leitrim LGBTQI+ community through the production of an annual community LGBTQI+ Pride Festival and events throughout the year.
+                    </p>
+                    <p>
+                        Leitrim Pride envisions a unified community where diverse individuals are celebrated and are able to thrive as their authentic selves.
+                    </p>
+                </div>
+            </div>
+        </div>
     );
 };

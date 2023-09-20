11364482d6af7d606117e931ac0caa5c1e33dae7
Sean Jacobson <sean.jacobson86@gmail.com>
2023-09-20 12:19:37 -0600

    Made the homePageLitP class; added font-size styling

--- src/components/homePage/literature.js
+++ src/components/homePage/literature.js
@@ -7,11 +7,14 @@ export default function HomePageLit() {
     return (
         <div className="container">
             <div className="row">
+                {/* Home Page literature */}
                 <div className="col-xl-12">
-                    <p>
+                    {/* Paragraph 1 */}
+                    <p className="homePageLitP">
                         Leitrim Pride seeks to bring together individuals and organisations to empower, educate and support the Leitrim LGBTQI+ community through the production of an annual community LGBTQI+ Pride Festival and events throughout the year.
                     </p>
-                    <p>
+                    {/* Paragraph 2 */}
+                    <p className="homePageLitP">
                         Leitrim Pride envisions a unified community where diverse individuals are celebrated and are able to thrive as their authentic selves.
                     </p>
                 </div>

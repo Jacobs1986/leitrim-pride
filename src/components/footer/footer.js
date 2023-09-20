940e3f89f55e052676c04f7d54839ecccc116200
Sean Jacobson <sean.jacobson86@gmail.com>
2023-09-20 12:40:06 -0600

    Added footer tag; created className footerIcon

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,6 +5,46 @@ import "./footer.css";
 
 export default function Footer() {
     return (
-        <div>This is the footer component.</div>
+        <footer>
+            <div className="container">
+                <div className="row">
+                    {/* Copyright */}
+                    <div className="col-xs-12">
+                        <span dangerouslySetInnerHTML={{ "__html": "&copy;" }} /> Leitrim Pride 2022
+                    </div>
+                    {/* Contact link */}
+                    <div className="col-xs-12">
+                        <a href="/contact">Contact Us</a>
+                    </div>
+                    {/* Social Media Links */}
+                    <div>
+                        {/* Facebook */}
+                        <a href="https://www.facebook.com/LeitrimPride" target={"_blank"} rel='noreferrer'>
+                            <img
+                                src="./icons/facebook.png"
+                                alt="Facebook Icon"
+                                className="footerIcon"
+                            />
+                        </a>
+                        {/* Instagram */}
+                        <a href="https://www.instagram.com/leitrimpride/" target={"_blank"} rel='noreferrer'>
+                            <img
+                                src="./icons/instagram.png"
+                                alt="Instagram Icon"
+                                className="footerIcon"
+                            />
+                        </a>
+                        {/* Twitter */}
+                        <a href="https://twitter.com/LeitrimPride" target={"_blank"} rel='noreferrer'>
+                            <img
+                                src="./icons/twitter.png"
+                                alt="Twitter Icon"
+                                className="footerIcon"
+                            />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </footer>
     );
 };

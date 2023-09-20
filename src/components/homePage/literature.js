b95994dc0a174e6df4fb6c12ba47a5ab15c6bd2a
Sean Jacobson <sean.jacobson86@gmail.com>
2023-09-20 11:31:20 -0600

    Created homePage component folder

---
+++ src/components/homePage/literature.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// CSS File
+import "./homePage-styles.css";
+
+export default function HomePageLit() {
+    return (
+        <div>This is the literature component.</div>
+    );
+};

e934172151c7fbd7e5620e7edb493b879e651ce3
Sean Jacobson <sean.jacobson86@gmail.com>
2023-09-20 15:26:30 -0600

    Getting everything setup on the about page

--- src/pages/about-page.js
+++ src/pages/about-page.js
@@ -2,6 +2,18 @@ import React from "react";
 
 export default function About() {
     return (
-        <div>This is the about page!</div>
+        <div>
+            {/* Page Title */}
+            <h1 style={{ textAlign: "center"}}>Who We Are</h1>
+            {/* Page Content */}
+            <div className="container">
+                <div className="row">
+                    {/* Image */}
+                    <div className="col-xs-12">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };

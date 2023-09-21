34d23659dc4b95d279eff23d79594a9354bcb64b
Sean Jacobson <sean.jacobson86@gmail.com>
2023-09-21 13:08:25 -0600

    Finished writing out the code that will display team member info

--- src/components/teamPage/teamInfo.js
+++ src/components/teamPage/teamInfo.js
@@ -10,10 +10,53 @@ export default function TeamInfo() {
     return (
         <div>
             {/* Map out teamInfo json */}
-            {/* Put it into a row */}
-            {/* First column should have the picture of the team member */}
-            {/* If there is no picture show a pride flag instead */}
-            {/* Second column will have the memeber's bio info */}
+            {teamInfo.map(member => (
+                <div>
+                    {/* Check to see if a member has a bio */}
+                    {!member.text ?
+                        // If not return an empty row
+                        <div className="row"></div> :
+                        // Begin a new row
+                        <div className="row bioRow" key={member.id}>
+                            {/* First column has the picture */}
+                            <div className="col-xs-12">
+                                {/* Check to see if there is an image */}
+                                {!member.image ?
+                                    // If no image show a pride flag
+                                    <img
+                                        src="./Images/pride-flag-01.webp"
+                                        alt="Pride Flag"
+                                        width="100%"
+                                    /> :
+                                    // Show provided image
+                                    <img
+                                        src={`./Images/${member.image}`}
+                                        alt={`${member.name} Headshot`}
+                                        width="100%"
+                                    />
+                                }
+                                {/* Member's name, title and preferred pronouns */}
+                                <div className="memberDiv">
+                                    <h2>
+                                        {member.name}
+                                    </h2>
+                                    <h4>
+                                        Title: {member.title}
+                                    </h4>
+                                    <h4>
+                                        Pronouns: {member.pronouns}
+                                    </h4>
+                                </div>
+                            </div>
+                            {/* Members biographical information */}
+                            <div className="col-xs-12">
+                                {/* Split the text so that it comes out in paragraphs */}
+                                {member.text.split("\n").map((paragraph, i) => <p key={i}>{paragraph}</p>)}
+                            </div>
+                        </div>
+                    }
+                </div>
+            ))}
         </div>
     );
 };

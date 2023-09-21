6cb033926da0bb1055ee5b9f507cbbf128605351
Sean Jacobson <sean.jacobson86@gmail.com>
2023-09-21 12:43:00 -0600

    Created a framework for the mapping function

--- src/components/teamPage/teamInfo.js
+++ src/components/teamPage/teamInfo.js
@@ -3,8 +3,17 @@ import React from "react";
 // CSS File
 import "./teamPage-styles.css";
 
+// Import teamInfo
+import teamInfo from "./teaminfo.json";
+
 export default function TeamInfo() {
     return (
-        <div>The team imfo will be displayed here.</div>
+        <div>
+            {/* Map out teamInfo json */}
+            {/* Put it into a row */}
+            {/* First column should have the picture of the team member */}
+            {/* If there is no picture show a pride flag instead */}
+            {/* Second column will have the memeber's bio info */}
+        </div>
     );
 };

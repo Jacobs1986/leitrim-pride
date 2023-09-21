a7ebac41b7842c5cc26d88de9b57b72cd239afd5
Sean Jacobson <sean.jacobson86@gmail.com>
2023-09-21 12:36:57 -0600

    Created teamPage folder that will have all of the components for the team-page

---
+++ src/components/teamPage/teamInfo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// CSS File
+import "./teamPage-styles.css";
+
+export default function TeamInfo() {
+    return (
+        <div>The team imfo will be displayed here.</div>
+    );
+};
--- src/pages/team-page.js
+++ src/pages/team-page.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+// Components
+import TeamInfo from "../components/teamPage/teamInfo";
+
 export default function Team() {
     return (
-        <div>This is the team page!</div>
+        <div>
+            {/* Page title */}
+            <h1 style={{ textAlign: "center"}}>
+                Meet Our Team
+            </h1>
+            {/* Team Info */}
+            <TeamInfo />
+        </div>
     );
 };

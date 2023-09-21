c2a579e7ef956a559bceb489f43243bf0b4b118f
Sean Jacobson <sean.jacobson86@gmail.com>
2023-09-21 16:39:01 -0600

    Created joinTheTeam component and imported into the page

--- src/pages/get-involved-page.js
+++ src/pages/get-involved-page.js
@@ -2,6 +2,7 @@ import React from "react";
 
 // Components
 import GetInvolvedBanner from "../components/getInvolvedPage/getInvolvedBanner";
+import JoinTheTeam from "../components/getInvolvedPage/joinTheTeam";
 
 export default function GetInvolved() {
     return (
@@ -11,6 +12,13 @@ export default function GetInvolved() {
                 Get Involved
             </h2>
             <GetInvolvedBanner />
+            {/* Page literature */}
+            <div className="row">
+                {/* Join the Team */}
+                <div className="col-xs-12">
+                    <JoinTheTeam />
+                </div>
+            </div>
         </div>
     );
 };
